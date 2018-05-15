d257f5880b00f1e118507a08b591b48878bc4796
Philippe Marti (CSCS) <philippe.marti@erdw.ethz.ch>
2018-05-15 17:14:55 +0200

    Update patches for vdiff_down

--- standalones/vdiff_down/scripts/apply_patches_extract_finish.sh
+++ standalones/vdiff_down/scripts/apply_patches_extract_finish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
 # vdiff_down FTG finish patches
-patch -p0 <${scriptdir}/patches/ftg_vdiff_down_test_finish_ftg.patch
+patch -p1 <${scriptdir}/patches/ftg_vdiff_down_test_finish_ftg.patch
--- standalones/vdiff_down/scripts/apply_patches_extract_intermezzo.sh
+++ standalones/vdiff_down/scripts/apply_patches_extract_intermezzo.sh
@@ -2,4 +2,4 @@
 
 # vdiff_down FTG intermezzo patches
 patch -p0 <${scriptdir}/patches/mo_convect_tables_intermezzo_ftg.patch
-patch -p0 <${scriptdir}/patches/mo_vdiff_downward_sweep_intermezzo_ftg.patch
+patch -p1 <${scriptdir}/patches/mo_vdiff_downward_sweep_intermezzo_ftg.patch
