2cce0b9d7b731347c8a7dc424ee87746c391b607
Philippe Marti (CSCS) <philippe.marti@erdw.ethz.ch>
2018-05-25 14:54:28 +0200

    Interactive steps for other two scripts

--- standalones/common/create_reference.sh
+++ standalones/common/create_reference.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 source ${commondir}/run_command.sh
+source ${commondir}/interactive_step.sh
 
 # Build compiler name for configure script by extending with target (except CPU)
 compiler_target="${compiler}"
@@ -8,40 +9,70 @@ if [ "${target}" = "gpu" ]; then
   compiler_target+="_${target}"
 fi
 
-# Apply patches for standalone
-run_command ${commondir}/apply_patches_reference.sh || exit 1
+# Allow for interactive interruption
+interactive_step "apply standalone run patches"
+if [[ ${run_next_part} -eq 1 ]]; then
 
-# Copy standalone files and tune for current install
-run_command cp -r ${scriptdir}/src . || exit 1
-while read -r f;do
-  run_command sed -i -e "s|++FTGDATADIR++|${ftgoutputdir}|g" "$f" || exit 1
-done <${scriptdir}/ftgdatadir_files.txt
+  # Apply patches for standalone
+  run_command ${commondir}/apply_patches_reference.sh || exit 1
 
-# Configure build
-run_command echo "Configuring standalone ..." || exit 1
-run_command ./configure --with-fortran="${compiler_target}" >& standalone_configure.log || exit 1
+fi
 
-# Build
-run_command echo "Building standalone ..." || exit 1
-run_command ./build_command >& standalone_build.log || exit 1
+# Allow for interactive interruption
+interactive_step "get standalone files and configure them"
+if [[ ${run_next_part} -eq 1 ]]; then
 
-# Make runscript
-run_command ${workdir}/make_runscripts || exit 1
-run_command sed -i -e "s/--ntasks-per-node=[0-9]*$/--ntasks-per-node=1/g" -e "s/mpi_procs_pernode=[0-9]*$/mpi_procs_pernode=1/g" run/exp.atm_amip_test.run || exit 1
-run_command cp ${commondir}/submit_reference.sh ${workdir}/ || exit 1
+  # Copy standalone files and tune for current install
+  run_command cp -r ${scriptdir}/src . || exit 1
+  while read -r f;do
+    run_command sed -i -e "s|++FTGDATADIR++|${ftgoutputdir}|g" "$f" || exit 1
+  done <${scriptdir}/ftgdatadir_files.txt
 
-# Run serialization
-while read -r nproma; do
+fi
 
-  # Prepare output directories
-  run_command mkdir -p "${ftgoutputdir}/nproma_${nproma}" || exit 1
-  run_command ln -s "${ftgoutputdir}/nproma_${nproma}" "${ftgoutputdir}/data" || exit 1
+# Allow for interactive interruption
+interactive_step "configure and build ICON"
+if [[ ${run_next_part} -eq 1 ]]; then
 
-  # Run executable
-  run_command echo "Running AMIP test with serialization ..."
-  run_command sed -i -e "s/nproma=[0-9]*$/nproma=${nproma}/g" run/exp.atm_amip_test.run || exit 1
-  run_command ./submit_reference.sh || exit 1
+  # Configure build
+  run_command echo "Configuring standalone ..." || exit 1
+  run_command ./configure --with-fortran="${compiler_target}" >& standalone_configure.log || exit 1
 
-  run_command rm "${ftgoutputdir}/data" || exit 1
+  # Build
+  run_command echo "Building standalone ..." || exit 1
+  run_command ./build_command >& standalone_build.log || exit 1
 
-done <${scriptdir}/nproma_list.txt
+fi
+
+# Allow for interactive interruption
+interactive_step "configure runscripts"
+if [[ ${run_next_part} -eq 1 ]]; then
+
+  # Make runscript
+  run_command ${workdir}/make_runscripts >& standalone_make_runscripts.log || exit 1
+  run_command sed -i -e "s/--ntasks-per-node=[0-9]*$/--ntasks-per-node=1/g" -e "s/mpi_procs_pernode=[0-9]*$/mpi_procs_pernode=1/g" run/exp.atm_amip_test.run || exit 1
+  run_command cp ${commondir}/submit_reference.sh ${workdir}/ || exit 1
+
+fi
+
+# Allow for interactive interruption
+interactive_step "run serialization"
+if [[ ${run_next_part} -eq 1 ]]; then
+
+  # Run serialization
+  while read -r nproma; do
+
+    # Prepare output directories
+    run_command mkdir -p "${ftgoutputdir}/nproma_${nproma}" || exit 1
+    run_command ln -s "${ftgoutputdir}/nproma_${nproma}" "${ftgoutputdir}/data" || exit 1
+
+    # Run executable
+    run_command echo "Running AMIP test with serialization ..."
+    run_command sed -i -e "s/nproma=[0-9]*$/nproma=${nproma}/g" run/exp.atm_amip_test.run || exit 1
+    run_command ./submit_reference.sh || exit 1
+
+    run_command rm "${ftgoutputdir}/data" || exit 1
+
+  done <${scriptdir}/nproma_list.txt
+
+fi
--- standalones/common/run_standalone_test.sh
+++ standalones/common/run_standalone_test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 source ${commondir}/run_command.sh
+source ${commondir}/interactive_step.sh
 
 # Build compiler name for configure script by extending with target (except CPU)
 compiler_target="${compiler}"
@@ -8,83 +9,120 @@ if [ "${target}" = "gpu" ]; then
   compiler_target+="_${target}"
 fi
 
-# Apply patches for standalone
-run_command ${commondir}/apply_patches_run.sh || exit 1
+# Allow for interactive interruption
+interactive_step "apply standalone run patches"
+if [[ ${run_next_part} -eq 1 ]]; then
 
-# Copy standalone files and tune for current install
-run_command cp -r ${scriptdir}/src . || exit 1
-while read -r f;do
-  run_command sed -i -e "s|++FTGDATADIR++|${ftgoutputdir}|g" "$f" || exit 1
-done <${scriptdir}/ftgdatadir_files.txt
+  # Apply patches for standalone
+  run_command ${commondir}/apply_patches_run.sh || exit 1
 
-# Remove existing ACC statements unrelated to standalone
-if [ -f ${scriptdir}/noacc_list.txt ]; then
-  while read -r f;do
-    run_command sed -i -e 's/!$ACC/!NOENIAC/g' -e 's/!$acc/!noeniac/g' "$f" || exit 1
-  done <${scriptdir}/noacc_list.txt
 fi
 
-# Configure build
-run_command echo "Configuring standalone ..." || exit 1
-run_command ./configure --with-fortran="${compiler_target}" >& standalone_configure.log || exit 1
-# Tune configuration for standalone
-${commondir}/tune_configuration.sh
+# Allow for interactive interruption
+interactive_step "get standalone files and configure them"
+if [[ ${run_next_part} -eq 1 ]]; then
 
-# Build
-run_command echo "Building standalone ..." || exit 1
-run_command ./build_command >& standalone_build.log || exit 1
+  # Copy standalone files and tune for current install
+  run_command cp -r ${scriptdir}/src . || exit 1
+  while read -r f;do
+    run_command sed -i -e "s|++FTGDATADIR++|${ftgoutputdir}|g" "$f" || exit 1
+  done <${scriptdir}/ftgdatadir_files.txt
 
-# Sync run data and prepare run
-run_command rsync -aqv "${datadir}/experiments" ${workdir} || exit 1
-run_command cp ${commondir}/submit.sh ${expdir} || exit 1
-run_command cd ${expdir} || exit 1
-run_command ln -sf ${workdir}/build/*/bin/${testname} . || exit 1
+fi
 
-# Run tests
-teststatus=0
-tid=0
-export nproma
-while read -r nproma; do
+# Allow for interactive interruption
+interactive_step "fix existing OpenACC statements"
+if [[ ${run_next_part} -eq 1 ]]; then
 
-  # Sync serialization data for FTG
-  run_command mkdir -p "${ftginputdir}" || exit 1
-  run_command mkdir -p "${ftgoutputdir}/nproma_${nproma}" || exit 1
-  run_command rsync -aqv "${dataftgdir}/nproma_${nproma}" "${ftginputdir}/" || exit 1
-  run_command ln -s "${ftgoutputdir}/nproma_${nproma}" "${ftgoutputdir}/data" || exit 1
-  run_command ln -s "${ftginputdir}/nproma_${nproma}/input" "${ftgoutputdir}/data/input" || exit 1
-  run_command ln -s "${ftginputdir}/nproma_${nproma}/output" "${ftgoutputdir}/data/output" || exit 1
-
-  # Run executable
-  run_command echo "Running standalone ..."
-  if [ -e ${scriptdir}/scripts/tune_run.sh ]; then
-    run_command ${scriptdir}/scripts/tune_run.sh || exit 1
-  fi
-  run_command ./submit.sh || exit 1
-
-  # Validate run
-  run_command ${commondir}/validate.sh || exit 1
-
-  if [ -f "validation_nproma_${nproma}.failed" ]; then
-    let "teststatus+=2**${tid}"
+  # Remove existing ACC statements unrelated to standalone
+  if [ -f ${scriptdir}/noacc_list.txt ]; then
+    while read -r f;do
+      run_command sed -i -e 's/!$ACC/!NOENIAC/g' -e 's/!$acc/!noeniac/g' "$f" || exit 1
+    done <${scriptdir}/noacc_list.txt
   fi
 
-  run_command rm "${ftgoutputdir}/data/input" || exit 1
-  run_command rm "${ftgoutputdir}/data/output" || exit 1
-  run_command rm "${ftgoutputdir}/data" || exit 1
-  let "tid+=1"
+fi
 
-done <${scriptdir}/nproma_list.txt
+# Allow for interactive interruption
+interactive_step "configure and build standalone"
+if [[ ${run_next_part} -eq 1 ]]; then
 
-# Final status report
-if [ "${teststatus}" -eq 0 ]; then
-      run_command echo "#-----------------------------#" || exit 1
-      run_command echo "#---   All tests PASSED!   ---#" || exit 1
-      run_command echo "#-----------------------------#" || exit 1
-  exit 0
-else
-      run_command echo "#------------------------------#" || exit 1
-      run_command echo "#---   Some tests FAILED:   ---#" || exit 1
-      run_command echo "#---   Status: " $(echo "obase=2;${teststatus}" | bc) "  ---#" || exit 1
-      run_command echo "#------------------------------#" || exit 1
-  exit 1
+  # Configure build
+  run_command echo "Configuring standalone ..." || exit 1
+  run_command ./configure --with-fortran="${compiler_target}" >& standalone_configure.log || exit 1
+  # Tune configuration for standalone
+  ${commondir}/tune_configuration.sh
+
+  # Build
+  run_command echo "Building standalone ..." || exit 1
+  run_command ./build_command >& standalone_build.log || exit 1
+
+fi
+
+# Allow for interactive interruption
+interactive_step "sync serialization data and prepare run"
+if [[ ${run_next_part} -eq 1 ]]; then
+
+  # Sync run data and prepare run
+  run_command rsync -aqv "${datadir}/experiments" ${workdir} || exit 1
+  run_command cp ${commondir}/submit.sh ${expdir} || exit 1
+  run_command cd ${expdir} || exit 1
+  run_command ln -sf ${workdir}/build/*/bin/${testname} . || exit 1
+
+fi
+
+
+# Allow for interactive interruption
+interactive_step "run validation tests"
+if [[ ${run_next_part} -eq 1 ]]; then
+
+  # Run tests
+  teststatus=0
+  tid=0
+  export nproma
+  while read -r nproma; do
+
+    # Sync serialization data for FTG
+    run_command mkdir -p "${ftginputdir}" || exit 1
+    run_command mkdir -p "${ftgoutputdir}/nproma_${nproma}" || exit 1
+    run_command rsync -aqv "${dataftgdir}/nproma_${nproma}" "${ftginputdir}/" || exit 1
+    run_command ln -s "${ftgoutputdir}/nproma_${nproma}" "${ftgoutputdir}/data" || exit 1
+    run_command ln -s "${ftginputdir}/nproma_${nproma}/input" "${ftgoutputdir}/data/input" || exit 1
+    run_command ln -s "${ftginputdir}/nproma_${nproma}/output" "${ftgoutputdir}/data/output" || exit 1
+
+    # Run executable
+    run_command echo "Running standalone ..."
+    if [ -e ${scriptdir}/scripts/tune_run.sh ]; then
+      run_command ${scriptdir}/scripts/tune_run.sh || exit 1
+    fi
+    run_command ./submit.sh || exit 1
+
+    # Validate run
+    run_command ${commondir}/validate.sh || exit 1
+
+    if [ -f "validation_nproma_${nproma}.failed" ]; then
+      let "teststatus+=2**${tid}"
+    fi
+
+    run_command rm "${ftgoutputdir}/data/input" || exit 1
+    run_command rm "${ftgoutputdir}/data/output" || exit 1
+    run_command rm "${ftgoutputdir}/data" || exit 1
+    let "tid+=1"
+
+  done <${scriptdir}/nproma_list.txt
+
+  # Final status report
+  if [ "${teststatus}" -eq 0 ]; then
+        run_command echo "#-----------------------------#" || exit 1
+        run_command echo "#---   All tests PASSED!   ---#" || exit 1
+        run_command echo "#-----------------------------#" || exit 1
+    exit 0
+  else
+        run_command echo "#------------------------------#" || exit 1
+        run_command echo "#---   Some tests FAILED:   ---#" || exit 1
+        run_command echo "#---   Status: " $(echo "obase=2;${teststatus}" | bc) "  ---#" || exit 1
+        run_command echo "#------------------------------#" || exit 1
+    exit 1
+  fi
+
 fi
