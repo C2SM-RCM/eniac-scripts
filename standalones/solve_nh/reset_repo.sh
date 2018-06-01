ae907f00c49fce84db89dd51e1b350398fffc3b2
William <wsawyer@cscs.ch>
2018-06-01 13:19:53 +0200

    first crack at solve_nh

---
+++ standalones/solve_nh/reset_repo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+# Environment setup
+workdir="$(pwd)"
+commondir="${workdir}/eniac-scripts/standalones/common"
+source ${commondir}/run_command.sh
+
+# Remove standalone test source
+rm -f "src/tests/ftg_solve_nh_test.f90"
+
+# More standalone specific cleanup
+#
+
+# General cleanup of repo
+run_command ${commondir}/reset_repo.sh || exit 1
