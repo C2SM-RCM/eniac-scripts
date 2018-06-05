5cc95932505c87f88d140434137ddbe1797ea4c6
Philippe Marti (CSCS) <philippe.marti@erdw.ethz.ch>
2018-06-05 12:34:31 +0200

    Added pythonpath for global Cheetah3

--- standalones/common/base_env.sh
+++ standalones/common/base_env.sh
@@ -8,6 +8,19 @@ dataftgdir="${datadir}/ftg/${compiler}/${slave}"
 fdepdir=/project/c14/data-eniac/standalones/fdependencies
 scriptdir="${workdir}/eniac-scripts/standalones/${testroutine}"
 
+# Check if global install of Cheetah3 is required
+need_cheetah=$(python -c 'import imp;imp.find_module("Cheetah")' >& /dev/null;echo $?)
+if [[ ${need_cheetah} == 1 ]];then
+  if [ "${slave}" == "kesch" ];then
+    CHEETAHPATH=/project/c14/install/kesch/cheetah3-3.1.0
+  elif [ "${slave}" == "daint" ];then
+    CHEETAHPATH=/apps/daint/UES/6.0.UP04/sandboxes/wsawyer/PYTHON/packages/lib/python2.7/site-packages/
+  fi
+  export PYTHONPATH=${PYTHONPATH}${PYTHONPATH+":"}${CHEETAHPATH}
+else
+  echo "Using local install of Cheetah3"
+fi
+
 export slave
 export compiler
 export target
