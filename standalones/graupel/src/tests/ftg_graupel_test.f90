1b0dafd137770644163209ad5168a5955bf4a688
Philippe Marti (CSCS) <philippe.marti@erdw.ethz.ch>
2018-10-24 10:20:40 +0200

    Update graupel patches

--- standalones/graupel/src/tests/ftg_graupel_test.f90
+++ standalones/graupel/src/tests/ftg_graupel_test.f90
@@ -112,14 +112,14 @@ CONTAINS
     &  ddt_diag_sdep, ddt_diag_agg, ddt_diag_rim, ddt_diag_rcri, ddt_diag_icri, ddt_diag_dau, ddt_diag_iau, ddt_diag_imelt, &
     &  ddt_diag_smelt, ddt_diag_cfrz, ddt_diag_rfrz, ddt_diag_shed, ddt_tend_qg)
     CALL ftg_destroy_serializer()
-   
-    CALL start_loc_timing("graupel", 1) 
+    
+    CALL start_loc_timing("graupel", 1)
     CALL graupel(nvec, ke, ivstart, ivend, kstart, idbg, zdt, dz, t, p, rho, qv, qc, qi, qr, qs, qg, qnc, qi0, qc0, prr_gsp, &
     &  prs_gsp, prg_gsp, qrsflux, l_cv, ldiag_ttend, ldiag_qtend, ddt_tend_t, ddt_tend_qv, ddt_tend_qc, &
     &  ddt_tend_qi, ddt_tend_qr, ddt_tend_qs, ddt_diag_au, ddt_diag_ac, ddt_diag_ev, ddt_diag_nuc, ddt_diag_idep, ddt_diag_sdep, &
     &  ddt_diag_agg, ddt_diag_rim, ddt_diag_rcri, ddt_diag_icri, ddt_diag_dau, ddt_diag_iau, ddt_diag_imelt, ddt_diag_smelt, &
     &  ddt_diag_cfrz, ddt_diag_rfrz, ddt_diag_shed, ddt_tend_qg)
-    CALL end_loc_timing(1) 
+    CALL end_loc_timing(1)
     CALL print_loc_timing()
     
   END SUBROUTINE ftg_test_graupel
@@ -138,7 +138,7 @@ CONTAINS
     IF (SERIALBOX_DEBUG) THEN
       CALL ftg_print_serializer_debuginfo()
     END IF
-
+    
     call init_loc_timing()
     
   END SUBROUTINE ftg_graupel_init_for_replay
@@ -219,22 +219,67 @@ CONTAINS
     CALL ftg_read("ke", ke)
     CALL ftg_read("zdt", zdt)
     CALL ftg_allocate_and_read_allocatable("dz", dz, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_dz)
+    !$ACC ENTER DATA COPYIN( dz )
+#endif
     CALL ftg_allocate_and_read_allocatable("t", t, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_t)
+    !$ACC ENTER DATA COPYIN( t )
+#endif
     CALL ftg_allocate_and_read_allocatable("p", p, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_p)
+    !$ACC ENTER DATA COPYIN( p )
+#endif
     CALL ftg_allocate_and_read_allocatable("rho", rho, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_rho)
+    !$ACC ENTER DATA COPYIN( rho )
+#endif
     CALL ftg_allocate_and_read_allocatable("qv", qv, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_qv)
+    !$ACC ENTER DATA COPYIN( qv )
+#endif
     CALL ftg_allocate_and_read_allocatable("qc", qc, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_qc)
+    !$ACC ENTER DATA COPYIN( qc )
+#endif
     CALL ftg_allocate_and_read_allocatable("qi", qi, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_qi)
+    !$ACC ENTER DATA COPYIN( qi )
+#endif
     CALL ftg_allocate_and_read_allocatable("qr", qr, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_qr)
+    !$ACC ENTER DATA COPYIN( qr )
+#endif
     CALL ftg_allocate_and_read_allocatable("qs", qs, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_qs)
+    !$ACC ENTER DATA COPYIN( qs )
+#endif
     CALL ftg_allocate_and_read_allocatable("qg", qg, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_qg)
+    !$ACC ENTER DATA COPYIN( qg )
+#endif
     CALL ftg_allocate_and_read_allocatable("qnc", qnc, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_qnc)
+    !$ACC ENTER DATA COPYIN( qnc )
+#endif
     CALL ftg_read("qi0", qi0)
     CALL ftg_read("qc0", qc0)
     CALL ftg_allocate_and_read_allocatable("prr_gsp", prr_gsp, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_prr_gsp)
+    !$ACC ENTER DATA COPYIN( prr_gsp )
+#endif
     CALL ftg_allocate_and_read_allocatable("prs_gsp", prs_gsp, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_prs_gsp)
+    !$ACC ENTER DATA COPYIN( prs_gsp )
+#endif
     CALL ftg_allocate_and_read_allocatable("prg_gsp", prg_gsp, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_prg_gsp)
+    !$ACC ENTER DATA COPYIN( prg_gsp )
+#endif
     CALL ftg_allocate_and_read_allocatable("qrsflux", qrsflux, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_qrsflux)
+    !$ACC ENTER DATA COPYIN( qrsflux )
+#endif
     
     ! OPTIONAL ARGUMENTS
     IF (PRESENT(ivstart)) THEN
@@ -260,75 +305,147 @@ CONTAINS
     END IF
     IF (PRESENT(ddt_tend_t)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_tend_t", ddt_tend_t, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_tend_t)
+      !$ACC ENTER DATA COPYIN( ddt_tend_t )
+#endif
     END IF
     IF (PRESENT(ddt_tend_qv)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_tend_qv", ddt_tend_qv, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_tend_qv)
+      !$ACC ENTER DATA COPYIN( ddt_tend_qv )
+#endif
     END IF
     IF (PRESENT(ddt_tend_qc)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_tend_qc", ddt_tend_qc, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_tend_qc)
+      !$ACC ENTER DATA COPYIN( ddt_tend_qc )
+#endif
     END IF
     IF (PRESENT(ddt_tend_qi)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_tend_qi", ddt_tend_qi, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_tend_qi)
+      !$ACC ENTER DATA COPYIN( ddt_tend_qi )
+#endif
     END IF
     IF (PRESENT(ddt_tend_qr)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_tend_qr", ddt_tend_qr, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_tend_qr)
+      !$ACC ENTER DATA COPYIN( ddt_tend_qr )
+#endif
     END IF
     IF (PRESENT(ddt_tend_qs)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_tend_qs", ddt_tend_qs, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_tend_qs)
+      !$ACC ENTER DATA COPYIN( ddt_tend_qs )
+#endif
     END IF
     IF (PRESENT(ddt_diag_au)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_diag_au", ddt_diag_au, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_diag_au)
+      !$ACC ENTER DATA COPYIN( ddt_diag_au )
+#endif
     END IF
     IF (PRESENT(ddt_diag_ac)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_diag_ac", ddt_diag_ac, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_diag_ac)
+      !$ACC ENTER DATA COPYIN( ddt_diag_ac )
+#endif
     END IF
     IF (PRESENT(ddt_diag_ev)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_diag_ev", ddt_diag_ev, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_diag_ev)
+      !$ACC ENTER DATA COPYIN( ddt_diag_ev )
+#endif
     END IF
     IF (PRESENT(ddt_diag_nuc)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_diag_nuc", ddt_diag_nuc, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_diag_nuc)
+      !$ACC ENTER DATA COPYIN( ddt_diag_nuc )
+#endif
     END IF
     IF (PRESENT(ddt_diag_idep)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_diag_idep", ddt_diag_idep, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_diag_idep)
+      !$ACC ENTER DATA COPYIN( ddt_diag_idep )
+#endif
     END IF
     IF (PRESENT(ddt_diag_sdep)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_diag_sdep", ddt_diag_sdep, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_diag_sdep)
+      !$ACC ENTER DATA COPYIN( ddt_diag_sdep )
+#endif
     END IF
     IF (PRESENT(ddt_diag_agg)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_diag_agg", ddt_diag_agg, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_diag_agg)
+      !$ACC ENTER DATA COPYIN( ddt_diag_agg )
+#endif
     END IF
     IF (PRESENT(ddt_diag_rim)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_diag_rim", ddt_diag_rim, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_diag_rim)
+      !$ACC ENTER DATA COPYIN( ddt_diag_rim )
+#endif
     END IF
     IF (PRESENT(ddt_diag_rcri)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_diag_rcri", ddt_diag_rcri, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_diag_rcri)
+      !$ACC ENTER DATA COPYIN( ddt_diag_rcri )
+#endif
     END IF
     IF (PRESENT(ddt_diag_icri)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_diag_icri", ddt_diag_icri, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_diag_icri)
+      !$ACC ENTER DATA COPYIN( ddt_diag_icri )
+#endif
     END IF
     IF (PRESENT(ddt_diag_dau)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_diag_dau", ddt_diag_dau, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_diag_dau)
+      !$ACC ENTER DATA COPYIN( ddt_diag_dau )
+#endif
     END IF
     IF (PRESENT(ddt_diag_iau)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_diag_iau", ddt_diag_iau, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_diag_iau)
+      !$ACC ENTER DATA COPYIN( ddt_diag_iau )
+#endif
     END IF
     IF (PRESENT(ddt_diag_imelt)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_diag_imelt", ddt_diag_imelt, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_diag_imelt)
+      !$ACC ENTER DATA COPYIN( ddt_diag_imelt )
+#endif
     END IF
     IF (PRESENT(ddt_diag_smelt)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_diag_smelt", ddt_diag_smelt, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_diag_smelt)
+      !$ACC ENTER DATA COPYIN( ddt_diag_smelt )
+#endif
     END IF
     IF (PRESENT(ddt_diag_cfrz)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_diag_cfrz", ddt_diag_cfrz, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_diag_cfrz)
+      !$ACC ENTER DATA COPYIN( ddt_diag_cfrz )
+#endif
     END IF
     IF (PRESENT(ddt_diag_rfrz)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_diag_rfrz", ddt_diag_rfrz, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_diag_rfrz)
+      !$ACC ENTER DATA COPYIN( ddt_diag_rfrz )
+#endif
     END IF
     IF (PRESENT(ddt_diag_shed)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_diag_shed", ddt_diag_shed, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_diag_shed)
+      !$ACC ENTER DATA COPYIN( ddt_diag_shed )
+#endif
     END IF
     IF (PRESENT(ddt_tend_qg)) THEN
       CALL ftg_allocate_and_read_allocatable("ddt_tend_qg", ddt_tend_qg, ftg_rperturb)
+#if defined(FTG_ACC_COPYIN) && !defined(FTG_ACC_NOCOPYIN_ddt_tend_qg)
+      !$ACC ENTER DATA COPYIN( ddt_tend_qg )
+#endif
     END IF
     
     ! TYPE MEMBERS
